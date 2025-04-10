54efd837588302d647860b9ba68564c5db6e67f8
siyanda sokhela <SiyandaSokhela@gmail.com>
2025-04-10 16:25:17 +0200

    adding the cart

---
+++ app/(root)/cart/page.tsx
@@ -0,0 +1,20 @@
+/** @format */
+
+import { getMyCart } from "@/lib/actions/cart.action";
+import CartTable from "./cart-table";
+
+export const metadata = {
+  title: "Shopping Cart",
+};
+
+const CartPage = async () => {
+  const cart = await getMyCart();
+
+  return (
+    <>
+      <CartTable cart={cart} />
+    </>
+  );
+};
+
+export default CartPage;
